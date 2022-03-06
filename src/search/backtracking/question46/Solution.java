07127569bdf42bcbef2eb3169ef65f55a454305a
rainyuniverse <1075635501@qq.com>
2022-03-06 20:07:07 +0800

    Update Solution.java

--- src/search/backtracking/question46/Solution.java
+++ src/search/backtracking/question46/Solution.java
@@ -2,9 +2,8 @@ package search.backtracking.question46;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//回溯法
 class Solution {
-
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         int[] visited = new int[nums.length];
@@ -16,6 +15,7 @@ class Solution {
         if(curNums.size() == nums.length){
             res.add(new ArrayList<Integer>(curNums));
             //res.add(curNums)这种写法不对？
+            //要先进行拷贝再放进去 否则的话地址中存储的内容不断变化 最终存进res值也会发生变化
             return;
         }
         for(int i = 0; i < nums.length; i++){
