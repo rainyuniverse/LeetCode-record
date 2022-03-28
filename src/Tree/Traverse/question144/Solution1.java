057b8f1019e88cd7ede8c9d611c3d5266784661d
rainyuniverse <1075635501@qq.com>
2022-03-28 10:38:18 +0800

    question144

--- src/Tree/Traverse/question144/Solution1.java
+++ src/Tree/Traverse/question144/Solution1.java
@@ -1,5 +1,36 @@
 package Tree.Traverse.question144;
 
-class Solution1 {
+import Tree.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+// 非递归
+/**
+ * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+ * 内存消耗：39.7 MB, 在所有 Java 提交中击败了24.89%的用户
+ */
+class Solution1 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null){
+            return new ArrayList<Integer>();
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            // 前序遍历 按照中左右的顺序遍历
+            TreeNode curNode = stack.pop();
+            res.add(curNode.val);
+            // 先进后出 右节点先进后出
+            if(curNode.right != null){
+                stack.push(curNode.right);
+            }
+            // 后进先出 左节点后进先出
+            if(curNode.left != null){
+                stack.push(curNode.left);
+            }
+        }
+        return res;
+    }
 }
--- src/Tree/Traverse/question144/question144.java
+++ src/Tree/Traverse/question144/question144.java
@@ -1,4 +1,21 @@
 package Tree.Traverse.question144;
 
+import Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class question144 {
+    public static void main(String[] args) {
+        TreeNode node2 = new TreeNode(2, null, null);
+        TreeNode node1 = new TreeNode(1, null, null);
+        TreeNode node3 = new TreeNode(3, node1, node2);
+
+        Solution1 solution1 = new Solution1();
+        List<Integer> list = new ArrayList<>();
+        list = solution1.preorderTraversal(node3);
+        for(Integer i: list){
+            System.out.print(i + " ");
+        }
+    }
 }
