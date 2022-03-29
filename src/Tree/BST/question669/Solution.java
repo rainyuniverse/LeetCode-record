1b156ea5021fc074ffaa4574143474e9557aa14e
rainyuniverse <1075635501@qq.com>
2022-03-29 23:30:13 +0800

    Update Solution.java

--- src/Tree/BST/question669/Solution.java
+++ src/Tree/BST/question669/Solution.java
@@ -12,11 +12,14 @@ class Solution {
             return root;
         }
         if(root.val < low){
+            // 如果当前节点值小于最小值 则剪去左子树 进入右子树继续进行判断
             return trimBST(root.right, low, high);
         }
         if(root.val > high){
+            // 如果当前节点值大于最大值 则剪去右子树 进入左子树继续进行判断
             return trimBST(root.left, low, high);
         }
+        // 如果当前节点值在区间内 则进入左右子树进行判断
         root.left = trimBST(root.left, low, high);
         root.right = trimBST(root.right, low, high);
         return root;
