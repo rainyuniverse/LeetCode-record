0ef2f72eb9398ce7f5ed768b59fe72d503375f01
rainyuniverse <1075635501@qq.com>
2022-04-06 11:43:21 +0800

    Update question789.java

--- src/Acwing/Basic/binarysearch/question789/question789.java
+++ src/Acwing/Basic/binarysearch/question789/question789.java
@@ -1,7 +1,7 @@
 package Acwing.Basic.binarysearch.question789;
 
 import java.util.Scanner;
-
+// 二分 模板题
 public class question789 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
